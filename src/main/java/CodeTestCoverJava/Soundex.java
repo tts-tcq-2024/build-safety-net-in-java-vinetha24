9d7f07f4f7c23f4f808d82903cc63085a90c014c
vinetha24 <113199878+vinetha24@users.noreply.github.com>
2024-09-15 22:25:23 +0530

    Update Soundex.java
    Code added

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -1,3 +1,11 @@
+package CodeTestCoverJava;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Soundex {
 
   private static final Map<Character, Character> SOUNDEXMAP = new HashMap<Character, Character>() {
@@ -33,15 +41,17 @@ public class Soundex {
   };
 
 
-public static void main(String[] args) {
-      System.out.println(generateSoundex("Rot"));
-    }
+  public static void main(String[] args) throws Exception {
+
+    System.out.println(generateSoundex("Rot"));
+    
+  }
 
   /**
    * @param string
    * @throws Exception
    */
-  private static String generateSoundex(String name)  {
+  public static String generateSoundex(String name)  {
       return name == null || name.isEmpty()?"":processSoundexCode(name.toUpperCase());
 
   }
@@ -64,7 +74,7 @@ public static void main(String[] args) {
   private static String processSoundexCode(String name)  {
     StringBuilder soundex = new StringBuilder();
     soundex.append(name.charAt(0));
-    try {
+    
     char prevCode = getSoundexCode(name.charAt(0));
     for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
       char code = getSoundexCode(name.charAt(i));
@@ -72,11 +82,9 @@ public static void main(String[] args) {
         soundex.append(code);
         prevCode = code;
       }
+     
     }
-    }
-    catch (Exception exception) {
-      return exception.getLocalizedMessage();
-    }
+   
     padSoundex(soundex);
 
     return soundex.toString();
@@ -88,13 +96,13 @@ public static void main(String[] args) {
    * @return
    * @throws Exception
    */
-  private static char getSoundexCode(char charAt) throws Exception {
+  private static char getSoundexCode(char charAt) {
     if (SOUNDEXMAP.get(charAt) != null) {
       return SOUNDEXMAP.get(charAt);
+    } 
+      return '0';
     }
-  
-      throw new Exception("Invalid Character");
 
   }
 
-}
+
